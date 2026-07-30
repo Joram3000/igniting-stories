758bbe0f37f539f3231306c8b44a7fbc8b15e79b
Joram Kroon <joramkroon@live.nl>
2026-07-30 15:19:12 +0200

    small cleanup

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,8 +3,6 @@ import Link from 'next/link'
 import {PortableText} from '@portabletext/react'
 
 import {AllPosts} from '@/app/components/Posts'
-import GetStartedCode from '@/app/components/GetStartedCode'
-import SideBySideIcons from '@/app/components/SideBySideIcons'
 import {settingsQuery} from '@/sanity/lib/queries'
 import {sanityFetch} from '@/sanity/lib/live'
 import {dataAttr} from '@/sanity/lib/utils'
@@ -45,7 +43,6 @@ export default async function Page() {
           </div>
         </div>
         <div className=" flex flex-col items-center">
-          <SideBySideIcons />
           <div className="container relative mx-auto max-w-2xl pb-20 pt-10 space-y-6 lg:max-w-4xl lg:px-12 flex flex-col items-center">
             <div className="prose sm:prose-lg md:prose-xl xl:prose-2xl text-gray-700 prose-a:text-gray-700 font-light text-center">
               {settings?.description && (
@@ -59,25 +56,6 @@ export default async function Page() {
                   <PortableText value={settings.description} />
                 </div>
               )}
-              <div className="flex items-center flex-col gap-4">
-                <GetStartedCode />
-                <Link
-                  href="https://www.sanity.io/docs"
-                  className="inline-flex text-brand text-xs md:text-sm underline hover:text-gray-900"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Sanity Documentation
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    className="w-4 h-4 ml-1 inline"
-                    fill="currentColor"
-                  >
-                    <path d="M10 6V8H5V19H16V14H18V20C18 20.5523 17.5523 21 17 21H4C3.44772 21 3 20.5523 3 20V7C3 6.44772 3.44772 6 4 6H10ZM21 3V12L17.206 8.207L11.2071 14.2071L9.79289 12.7929L15.792 6.793L12 3H21Z"></path>
-                  </svg>
-                </Link>
-              </div>
             </div>
           </div>
         </div>
